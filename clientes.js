f1cf4405465f395de863a55c4e896b08a63c272b
trafegodados <palmapesquisa@gmail.com>
2025-08-09 20:30:53 -0300

    Create clientes.js

---
+++ clientes.js
@@ -0,0 +1,25 @@
+{
+  "corretores": {
+    "corretor1_id": {
+      "nome": "João Silva",
+      "email": "joao@imobiliaria.com"
+    }
+  },
+  "leads": {
+    "lead1_id": {
+      "nome": "Cliente A",
+      "cpf": "123.456.789-00",
+      "telefone": "(11) 99999-9999",
+      "email": "cliente@email.com",
+      "qualidade": "quente",
+      "corretor_id": "corretor1_id",
+      "ultimo_contato": "2023-11-20T10:00:00",
+      "observacoes": {
+        "atendimento1_id": {
+          "data": "2023-11-20T10:00:00",
+          "texto": "Cliente interessado no apartamento XYZ"
+        }
+      }
+    }
+  }
+}
